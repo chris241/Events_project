fb363fc7b6480876261b2c00fe533b4de85b0dc6
PaulMouchel <paulmouchel@live.fr>
2019-08-12 11:56:49 +0200

    Add seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(email:"paulmouchel@yopmail.com", description:"Salut, moi c'est Paul, je fais THP", first_name:"Paul", last_name:"Mouchel")
+User.create(email:"pierre.michel@yopmail.com", description:"Salut, moi c'est Pierre, je fais THP", first_name:"Pierre", last_name:"Michel")
+
