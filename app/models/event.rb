ea76f71da901831994b657f2ee81ab6c64919a20
PaulMouchel <paulmouchel@live.fr>
2019-08-12 14:01:40 +0200

    Fix issues in event.rb

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,11 +1,11 @@
 class Event < ApplicationRecord
 	validates :start_date, 
-		presence: true,
-		:in_the_future?
+		presence: true
+	validates :in_the_future?
   validates :duration,
     presence: true,
-    numericality: { only_integer: true, greater_than: 0},
-    :multiple_of_5?
+    numericality: { only_integer: true, greater_than: 0}
+  validates :multiple_of_5?
   validates :title, 
   	presence: true, 
   	length: { in: 5..140 }
@@ -23,9 +23,15 @@ class Event < ApplicationRecord
 end
 
 def multiple_of_5?
-	return self.duration % 5 == 0
+	#return self.duration % 5 == 0
+	if self.duration.present? && self.duration % 5 != 0
+      errors.add(:duration, "must be multiple of 5")
+  end
 end
 
 def in_the_future?
-	return self.start_date > Time.now
+	#return self.start_date > Time.now
+	if self.start_date.present? && self.start_date < Time.now
+      errors.add(:start_date, "can't be in the past")
+  end
 end
