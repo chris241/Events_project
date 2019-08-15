eddf4b7e9644dd640eaa1713f27e04aea0e70fdf
PaulMouchel <paulmouchel@live.fr>
2019-08-15 17:51:54 +0200

    Improve design

--- app/controllers/user_pics_controller.rb
+++ app/controllers/user_pics_controller.rb
@@ -2,6 +2,6 @@ class UserPicsController < ApplicationController
 	def create
     @user = User.find(params[:user_id])
     @user.user_pic.attach(params[:user_pic])
-    redirect_to(user_path(@user))
+    redirect_to(edit_user_path(@user))
   end
 end
